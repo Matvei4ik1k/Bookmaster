22eec423fa4d42b331ff681ed4aca7ce44867dc7
Дьячков.Матвей <.@TERM1>
2025-01-23 09:39:00 +0300

    Добавили переходы

--- View/Windows/MainWindow.xaml.cs
+++ View/Windows/MainWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Bookmaster.View.Pages;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Bookmaster.View.Windows
 {
@@ -23,5 +12,66 @@ namespace Bookmaster.View.Windows
         {
             InitializeComponent();
         }
+
+        private void LoginMi_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.ShowDialog();
+        }
+
+        private void LogOutMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CloseMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BrowseCatalogMi_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrm.Navigate(new BrowserCatalogPage());
+        }
+
+        private void ManageCustomersMi_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrm.Navigate(new ManageCustomersPage());
+
+        }
+
+        private void CirculationMi_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrm.Navigate(new CirculationPage());
+
+        }
+
+        private void ReportsMi_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrm.Navigate(new ReportsPage());
+
+        }
     }
 }
+
+
+// #Способы навигации в WPF
+#region 1) Оконная навигация
+// Осуществляет открытие нового экземпляра окна из другого окна или страницы.
+// Алгоритм для реализации (окно называется TestWindows.xaml):
+
+// - создать экземпляр окна TestWindows.xaml
+// - у экземпляра окна вызываем метод Show() данный метод открывает модальное окно ( будет возможность взаимодействовать с предыдущим окном ). - Иначе вызываем метод ShowDialog, данный метод открывает диалоговое окно ( нельзя взаимодействовать с предыдущим окном )
+
+// TestWindow testWindow = new TestWindow();
+// testWindow.Show();
+#endregion
+
+#region 2) Страничная навигация
+//Осуществляет окртытие страниц внутри элемента Fram.
+//// Алгоритм для реализации (страница называется TestPage.xaml):
+// - определить место для элемента Frame (данный элемент принимает и отображает страницы)
+// - дать имя фрэйму (MainFrame) в XAML - коде
+// - для навигации обратиться к фрэйму, вызвать метод Navigate и передать в качестве значения экземпляр страницы 
+// => MainFrm.Navigate(new TestPage());
+#endregion
