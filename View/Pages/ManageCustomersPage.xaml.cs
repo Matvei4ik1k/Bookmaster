417f21b521358be6b97352ee6f7ca077cc10c8e0
Дьячков.Матвей <.@TERM1>
2025-05-28 16:30:13 +0300

    ...

--- View/Pages/ManageCustomersPage.xaml.cs
+++ View/Pages/ManageCustomersPage.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using Bookmaster.View.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Bookmaster.View.Pages
 {
@@ -23,6 +11,12 @@ namespace Bookmaster.View.Pages
         public ManageCustomersPage()
         {
             InitializeComponent();
+        }
+
+        private void AddBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            EditCustomer editCustomer = new EditCustomer();
+            editCustomer.Show();
         }
     }
 }
